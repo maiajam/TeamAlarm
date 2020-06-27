000232ed05fb26f4226774601dd8148c2a9cfb05
mai waleed <maiajam88@hotmail.com>
2020-06-27 11:08:11 +0300

    Initial commit

---
+++ Objectdetails/app/src/main/java/com/maiajam/objectdetails/MainActivity2.kt
@@ -0,0 +1,16 @@
+package com.maiajam.objectdetails
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+    }
+}
